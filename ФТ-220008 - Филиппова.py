b6ed225f8bcb2ff65c84073a9d4b467489328489
kristinaphilippova <146865479+kristinaphilippova@users.noreply.github.com>
2023-11-05 15:26:20 +0500

    BugFix ФТ-220008 - Филиппова.py
    Исправление ошибки в расчёте весовых коэффициентов.

--- ФТ-220008 - Филиппова.py
+++ ФТ-220008 - Филиппова.py
@@ -1,37 +1,45 @@
-#Вычисление весовых коэффициентов критериев в анализе парных сравнений. 
-def calculate_weights(num_criteria):
-    pairwise_matrix = [[0 for _ in range(num_criteria)] for _ in range(num_criteria)]
-#Заполнение матрицы. Начиная с первого критерия, пользователю предлагается ввести значение сравнения для каждой пары критериев, и эти значения сохраняются в матрицу `pairwise_matrix`.
-    for i in range(num_criteria):
-        for j in range(i+1, num_criteria):
+def calculate_weights(n_criteria):
+    criteria = []
+    weights = []
+    total = 0
+
+    for i in range(n_criteria):
+        while True:
             try:
-                value = float(input(f"Введите значение сравнения критерия {i+1} и {j+1}: "))
-                pairwise_matrix[i][j] = value
-                pairwise_matrix[j][i] = 1 / value
-            #Контроль правильности ввода
+                comparison_value = float(input(f"Введите значение сравнения для критерия {i + 1}: "))
+                break
             except ValueError:
                 print("Ошибка ввода! Попробуйте снова.")
-                return
 
-    sum_columns = [sum(column) for column in zip(*pairwise_matrix)]
-    #Вычисление для нормализованной матрицы `normalized_matrix`
-    normalized_matrix = [[value/sum_col for value in row] for row, sum_col in zip(pairwise_matrix, sum_columns)]
-    #Вычисление весовых коэффициентов путем усреднения значений в строках нормализованной матрицы. Весовой коэффициент для критерия `i+1` равен среднему значению в строке `i`.
-    weights = [sum(row) / num_criteria for row in normalized_matrix]
+        criteria.append(comparison_value)
+        total += comparison_value
+
+    # Проверка на ноль и коррекция суммы критериев
+    if total == 0:
+        print("Ошибка: сумма критериев не может быть равна нулю.")
+        return None
+
+    for i in range(n_criteria):
+        weight = criteria[i] / total
+        weights.append(weight)
 
     return weights
 
-#Ввод количества критериев пользователем и вызов функции `calculate_weights` для расчета весовых коэффициентов
+
 def main():
-    try:
-        num_criteria = int(input("Введите количество критериев: "))
-        weights = calculate_weights(num_criteria)
+    while True:
+        try:
+            n = int(input("Введите количество критериев: "))
+            break
+        except ValueError:
+            print("Ошибка ввода! Попробуйте снова.")
+
+    result = calculate_weights(n)
+
+    if result:
         print("Весовые коэффициенты:")
-        for weight in weights:
+        for weight in result:
             print(f"{weight:.2f}")
-    #Контроль правильности ввода
-    except ValueError:
-        print("Ошибка ввода! Проверьте правильность введенного количества критериев.")
 
 
 if __name__ == "__main__":
